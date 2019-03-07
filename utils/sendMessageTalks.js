0a38f6d6def911c62911fe0d1248a3841f5f24c6
rushi7997 <ramirushi7997@gmail.com>
2019-03-08 02:22:58 +0530

    added script to send messages to all participants in  bvm talks for online certificates

---
+++ utils/sendMessageTalks.js
@@ -0,0 +1,60 @@
+const httpRequest = require('request-promise-native');
+require('request');
+
+let MongoClient =require('mongodb').MongoClient;
+let dotenv =require('dotenv');
+
+dotenv.config();
+
+function sendMessage(contacts){
+    const sender = process.env.SMS_SENDER;
+    const apiKey = process.env.SMS_API_KEY;
+    const test = process.env.SMS_TEST === 'true';
+    const numbers = contacts.join(",");
+    const message = "All the participants who have registered for BVM Talks have to open this link for receiving your certificate. \n" +
+        "Link: https://certificates.udaan19.in/";
+    const apiRequest = {
+        url: 'http://api.textlocal.in/send',
+        form: {
+            apiKey,
+            numbers,
+            test,
+            sender,
+            // custom: eventID,
+            message
+            // receiptUrl: 'http://udaan18-messenger.herokuapp.com/textlocal'
+        }
+    };
+    httpRequest.post(apiRequest).then((res)=>{
+        result=JSON.parse(res);
+        var success=[];
+        // console.log(result);
+        result.messages.forEach((message)=>{
+            success.push(message.recipient.toString().substring(2));
+            // console.log(success)
+        });
+        if (result.status==="success") {
+            console.log("success");
+        } else {
+            console.log("failure");
+        }
+
+    }).catch((err)=>{
+        console.log(err)
+    })
+}
+
+(async ()=>{
+    const client = await MongoClient.connect(process.env.DB, { useNewUrlParser: true});
+    const db = client.db('Udaan-19');
+
+    let data = await db.collection('Bvm-Talks').find({}).toArray();
+    // let phones = ['7069307537'];
+    let phones = [];
+
+    // data.forEach(obj=>{
+    //     phones.push(obj.phone);
+    // });
+    sendMessage(phones);
+
+})();
